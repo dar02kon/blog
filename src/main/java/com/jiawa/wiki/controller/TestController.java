d36bacbd9b0fdaca83059b530eeec01eee52d42d
daring02 <1482882482@qq.com>
2021-05-19 20:23:38 +0800

    3-4 mybatis使用示例：开发/test/list接口，查询test表所有数据

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,15 +1,24 @@
 package com.jiawa.wiki.controller;
 
+import com.jiawa.wiki.domain.Test;
+import com.jiawa.wiki.service.TestService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class TestController {
 
     @Value("${test.hello:TEST}")
     private String testHello;
+
+    @Resource
+    private TestService testService;
+
     @GetMapping("/hello")
     public String hello(){
         return "hello world" + testHello;
@@ -18,5 +27,11 @@ public class TestController {
     @PostMapping("/hello/post")
     public String helloPost(String name){
         return "hello world! Post" + name;
+    }
+
+    @GetMapping("/test/list")
+    public List<Test> list(){
+
+        return testService.list();
     }
 }
