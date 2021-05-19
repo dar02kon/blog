3844753a2a43a4c83a68319509d01bc831dc98a3
daring02 <1482882482@qq.com>
2021-05-19 21:48:23 +0800

    3-6 增加通用的返回类CommonResp，以后后端所有接口的返回值全部都是CommonResp类型

--- src/main/java/com/jiawa/wiki/controller/EbookController.java
+++ src/main/java/com/jiawa/wiki/controller/EbookController.java
@@ -1,11 +1,11 @@
 package com.jiawa.wiki.controller;
 
 import com.jiawa.wiki.domain.Ebook;
+import com.jiawa.wiki.resp.CommonResp;
 import com.jiawa.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.annotation.Resource;
 import java.util.List;
 
@@ -17,8 +17,12 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list(){
+    public CommonResp<List<Ebook>> list(){
 
-        return ebookService.list();
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
+
     }
 }
