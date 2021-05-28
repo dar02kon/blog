dea39665ca8422580adf4f224debf8b54d5e89f9
daring02 <1482882482@qq.com>
2021-05-28 19:49:05 +0800

    5-4 列表成员接口支持动态SQL name参数有值时按name查，没值时查全部 封面小图样式调整

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -94,7 +94,7 @@ export default defineComponent({
 
     onMounted(() => {
       console.log("onMounted")
-      axios.get("http://localhost:8080/ebook/list?name=Spring").then((response) => {
+      axios.get("http://localhost:8080/ebook/list").then((response) => {
        const data = response.data;
        ebooks.value = data.content;
        ebooks1.books = data.content;
@@ -121,3 +121,13 @@ export default defineComponent({
   }
 });
 </script>
+
+<style scoped>
+.ant-avatar {
+  width: 50px;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 8%;
+  margin: 5px 0;
+}
+</style>
