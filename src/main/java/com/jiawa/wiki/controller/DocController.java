345c50baf48e922983559793e65053007be1b7bc
daring02 <1482882482@qq.com>
2021-06-14 16:17:38 +0800

    8-6 删除某个文档时，将其下的所有文档全部删除

--- src/main/java/com/jiawa/wiki/controller/DocController.java
+++ src/main/java/com/jiawa/wiki/controller/DocController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.Arrays;
 import java.util.List;
 
 @JsonSerialize(using = ToStringSerializer.class)
@@ -47,11 +48,12 @@ public class DocController {
         docService.save(req);
         return resp;
     }
-    @DeleteMapping("/delete/{id}")
-    public CommonResp delete(@PathVariable Long id){
+    @DeleteMapping("/delete/{idsStr}")
+    public CommonResp delete(@PathVariable String idsStr){
 
         CommonResp resp = new CommonResp<>();
-        docService.delete(id);
+        List<String> list = Arrays.asList(idsStr.split(","));
+        docService.delete(list);
         return resp;
     }
 }
