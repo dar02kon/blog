680a41ae008fb4fd4556fcbecafd791d6ce4f965
daring02 <1482882482@qq.com>
2021-07-01 12:37:02 +0800

    9-9 使用vuex保存用户信息，方便其它组件获取登录信息。遗留问题，刷新后值没有了

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,13 +1,21 @@
 <template>
   <a-layout-footer style="text-align: center">
-    K-ON电子书
+    K-ON电子书 欢迎:{{user.name}}
   </a-layout-footer>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, computed} from 'vue';
+import store from "@/store";
 
  export default defineComponent({
-  name: 'the-footer'
+  name: 'the-footer',
+   setup() {
+     const user = computed(() => store.state.user);
+
+     return{
+       user
+     }
+   }
 });
 </script>
--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -50,6 +50,7 @@
 import { defineComponent,ref } from 'vue';
 import axios from 'axios';
 import { message } from 'ant-design-vue';
+import store from "@/store";
 
 declare let hexMd5: any;
 declare let KEY: any;
@@ -83,6 +84,7 @@ declare let KEY: any;
            loginModalVisible.value = false;
            message.success("登录成功！");
            user.value = data.content;
+           store.commit("setUser", user.value);
          } else {
            message.error(data.message);
          }
