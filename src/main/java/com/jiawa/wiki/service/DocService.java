fc79999f453ac7264dd7d0d842b1ec5ccab27436
daring02 <1482882482@qq.com>
2021-07-02 06:21:18 +0800

    10-9 完成点赞通知功能 点赞时，组装消息内容，往WS推送 前端收到WS消息后，弹出消息内容

--- src/main/java/com/jiawa/wiki/service/DocService.java
+++ src/main/java/com/jiawa/wiki/service/DocService.java
@@ -18,6 +18,7 @@ import com.jiawa.wiki.util.CopyUtil;
 import com.jiawa.wiki.util.RedisUtil;
 import com.jiawa.wiki.util.RequestContext;
 import com.jiawa.wiki.util.SnowFlake;
+import com.jiawa.wiki.websocket.WebSocketServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -43,6 +44,9 @@ public class DocService {
 
     @Resource
     private RedisUtil redisUtil;
+
+    @Resource
+    private WebSocketServer webSocketServer;
 
     public List<DocQueryResp> all(Long ebookId){
         DocExample docExample = new DocExample();
@@ -142,6 +146,10 @@ public class DocService {
         } else {
             throw new BusinessException(BusinessExceptionCode.VOTE_REPEAT);
         }
+
+        //推送消息
+        Doc docDb = docMapper.selectByPrimaryKey(id);
+        webSocketServer.sendInfo("【" + docDb.getName() + "】被点赞");
 
     }
 
