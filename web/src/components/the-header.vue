bfbe5d86ed9ff829765f79af34171880fe677774
daring02 <1482882482@qq.com>
2021-07-01 12:25:02 +0800

    9-9 前端显示登录昵称，遗留问题，刷新会没有

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -21,7 +21,10 @@
       <a-menu-item key="/about">
         <router-link to="/about">关于我们</router-link>
       </a-menu-item>
-      <a class="login-menu" @click="showLoginModal">
+      <a class="login-menu" v-show="user.id">
+        <span>您好：{{user.name}}</span>
+      </a>
+      <a class="login-menu" v-show="!user.id" @click="showLoginModal">
         <span>登录</span>
       </a>
     </a-menu>
@@ -54,10 +57,15 @@ declare let KEY: any;
  export default defineComponent({
   name: 'the-header',
    setup () {
+    //登录后保存
+     const user = ref();
+     user.value = {};
+     //用来登录
      const loginUser = ref({
        loginName: "test",
-       password: "test"
+       password: "123"
      });
+
      const loginModalVisible = ref(false);
      const loginModalLoading = ref(false);
      const showLoginModal = () => {
@@ -74,6 +82,7 @@ declare let KEY: any;
          if (data.success) {
            loginModalVisible.value = false;
            message.success("登录成功！");
+           user.value = data.content;
          } else {
            message.error(data.message);
          }
@@ -84,7 +93,8 @@ declare let KEY: any;
        loginModalLoading,
        showLoginModal,
        loginUser,
-       login
+       login,
+       user
      }
    }
  });
