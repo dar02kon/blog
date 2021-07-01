ff45cd1afa5390fcf77b1e9a7a781ffa39de5d66
daring02 <1482882482@qq.com>
2021-07-01 14:04:11 +0800

    9-12 增加路由登录拦截，未登录时跳转到首页

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -6,6 +6,8 @@ import AdminEbook from '../views/admin/admin-ebook.vue'
 import AdminUser from '../views/admin/admin-user.vue'
 import AdminCategory from '../views/admin/admin-category.vue'
 import AdminDoc from '../views/admin/admin-doc.vue'
+import {Tool} from "@/util/tool";
+import store from "@/store";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -23,35 +25,66 @@ const routes: Array<RouteRecordRaw> = [
     // component: () => import(/* webpackChunkName: "about" */ '../views/about.vue')
   },
   {
+    path: '/doc',
+    name: 'Doc',
+    component: Doc
+  },
+  {
     path: '/admin/ebook',
     name: 'AdminEbook',
-    component: AdminEbook
+    component: AdminEbook,
+    meta: {
+      loginRequire: true
+    }
   },
   {
     path: '/admin/user',
     name: 'AdminUser',
-    component: AdminUser
+    component: AdminUser,
+    meta: {
+      loginRequire: true
+    }
   },
   {
     path: '/admin/category',
     name: 'AdminCategory',
-    component: AdminCategory
+    component: AdminCategory,
+    meta: {
+      loginRequire: true
+    }
   },
   {
     path: '/admin/doc',
     name: 'AdminDoc',
     component: AdminDoc,
+    meta: {
+      loginRequire: true
+    }
   },
-  {
-    path: '/doc',
-    name: 'Doc',
-    component: Doc
-  },
+
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+// 路由登录拦截
+router.beforeEach((to, from, next) => {
+  // 要不要对meta.loginRequire属性做监控拦截
+  if (to.matched.some(function (item) {
+    console.log(item, "是否需要登录校验：", item.meta.loginRequire);
+    return item.meta.loginRequire
+  })) {
+    const loginUser = store.state.user;
+    if (Tool.isEmpty(loginUser)) {
+      console.log("用户未登录！");
+      next('/');
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
 
 export default router
