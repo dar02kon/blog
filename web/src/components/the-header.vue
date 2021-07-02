a49a9e9d1e259bd1e994b92cd93645b67b0dd6e1
daring02 <1482882482@qq.com>
2021-07-02 13:58:03 +0800

    11-9 1.修改LOGO展示 2.首次加载，给出等待提示

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -1,6 +1,6 @@
 <template>
   <a-layout-header class="header">
-    <div class="logo" />
+    <div class="logo">K-ON</div>
     <a-menu
         theme="dark"
         mode="horizontal"
@@ -128,6 +128,15 @@ declare let KEY: any;
 </script>
 
 <style>
+.logo {
+  width: 120px;
+  height: 31px;
+  /*background: rgba(255, 255, 255, 0.2);*/
+  /*margin: 16px 28px 16px 0;*/
+  float: left;
+  color: white;
+  font-size: 18px;
+}
 .login-menu {
   float: right;
   color: white;
