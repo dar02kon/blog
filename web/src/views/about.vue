f59d52322c41212ca59e07b51ca309a4dc483cf7
daring02 <1482882482@qq.com>
2021-07-02 14:14:23 +0800

    11-9 关于我们文案修改

--- web/src/views/about.vue
+++ web/src/views/about.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>あなたも舞のことを思い出しますか</h1>
+    <div>
+      <img src="/image/02-2.jpg">
+    </div>
+    <h1>それだけなのか</h1>
+    <div>
+      <img src="/image/玉2.jpg">
+    </div>
   </div>
 </template>
